a9666a283bbb71f42703aaad65141fcc9ab6086e
Heracraft <heracraftgamer.ne@gmail.com>
2024-07-20 00:03:34 +0300

    FEAT: Math graph with base functionality

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { createRootRoute, Outlet, Link } from "@tanstack/react-router";
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<div className="flex justify-end gap-2 sm:gap-5 p-5 bg-white dark:bg-zinc-900 [&>a]:text-sky-600 dark:[&>a]:text-sky-300 ">
+			<div className="flex justify-end gap-2 sm:gap-5 p-5 bg-white dark:bg-zinc-900 [&>a]:text-zinc-700 dark:[&>a]:text-white ">
 				<Link to="/">Home</Link>
 				<Link to="/lessons/math">Math Lessons</Link>
 				<Link to="/lessons/physics">Physics Lessons</Link>
--- client/src/routes/lessons/math.lazy.tsx
+++ client/src/routes/lessons/math.lazy.tsx
@@ -1,40 +1,62 @@
+import { useRef } from "react";
+import { createPortal } from "react-dom";
+
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { Mafs, Plot, Point, Coordinates, useMovablePoint } from "mafs";
 import range from "lodash/range";
+import Latex from "react-latex-next";
 
 import "mafs/core.css";
+// TODO: review latex compatability issues with safari. Safari a bitch
+import "katex/dist/katex.min.css";
 
 export const Route = createLazyFileRoute("/lessons/math")({
 	component: MathLesson,
 });
 
-function PointsAlongFunction() {
-	const fn = (x: number) => (x / 2) ** 2;
-	const sep = useMovablePoint([1, 0], {
-		constrain: "horizontal",
-	});
+// MAYBE: refine prop types
+function PointsAlongFunction(props: any) {
+	const sep = useMovablePoint([1, 0]);
+	let { formulaContainer } = props;
+	console.log(formulaContainer);
+
+	const fn = (x: number) => (x - sep.x) ** 2 + sep.y;
+	const axisOfSymmetry = (x: number) => sep.x;
 
 	const n = 10;
 
-	const points = sep.x != 0 ? range(-n * sep.x, (n + 0.5) * sep.x, sep.x) : [];
+	let pointsArgument = 2;
+
+	const points = range(-pointsArgument * pointsArgument, (pointsArgument + 0.5) * pointsArgument, pointsArgument);
+
+	let formulaLatexString = "f(x)=" + (Math.round(Math.abs(sep.x)) > 0 ? `(x^2 + ${Math.round(sep.x)})` : "x^2") + (Math.round(Math.abs(sep.y)) > 0 ? "+" + Math.round(sep.y) : "");
 
 	return (
-		<Mafs width={200} viewBox={{ x: [0, 0], y: [-1.3, 4.7] }}>
-			<Coordinates.Cartesian />
+		<>
+			<Latex>${formulaLatexString}$</Latex>
+			<Mafs zoom={false} viewBox={{ x: [-6, 6], y: [0, 5] }}>
+				{/* Tried using the provided LaTeX component (from Mafs) but it must render inside the graph which is too limiting */}
+				{/* <LaTeX at={[-4, 2]} tex={`f(x)=${sep.x > 0 ? "(x^2+" + Math.round(sep.x)+")" : "x^2"}+1`} /> */}
+				<Coordinates.Cartesian xAxis={{ lines: false }} yAxis={{ lines: false }} />
 
-			<Plot.OfX y={fn} opacity={0.25} />
-			{points.map((x, index) => (
+				<Plot.OfX y={fn} opacity={0.25} />
+				<Plot.OfY x={axisOfSymmetry} style="dashed" opacity={0.25} />
+				{/* {points.map((x, index) => (
 				<Point x={x} y={fn(x)} key={index} />
-			))}
-			{sep.element}
-		</Mafs>
+			))} */}
+				{sep.element}
+			</Mafs>
+		</>
 	);
 }
 
 function MathLesson() {
+	const formulaContainer = useRef(null);
 	return (
-		<div className="flex flex-col gap-4">
-			<PointsAlongFunction />
-		</div>
+		<main className="p-10">
+			<div className="flex flex-col gap-4 w-96" ref={formulaContainer}>
+				<PointsAlongFunction formulaContainer={formulaContainer} />
+			</div>
+		</main>
 	);
 }
